82c035b7a402090a725f5d091a2ca82ee046000f
jasonwkf <jasonwkf@sph.com.sg>
2023-04-21 14:37:27 +0800

    delete all css and js

--- src/js/page/plan-selection.js
+++ src/js/page/plan-selection.js
@@ -13,33 +13,8 @@ $(function () {
       }, 1000);
     }
   });
-  // plan selection
-  $('.pub-plan-single > .cta-button').on('click', function (e) {
-    e.preventDefault();
-    // code here
-  });
 
-  // magazine selection
-  $('.magazine-plan-single > .cta-button').on('click', function (e) {
-    e.preventDefault();
-    // code here
-  });
-
-  $(".dropdown-select-selected.mutli-select-pub .selected").on("click", function () {
-    $(".dropdown-select-options.mutli-select-pub").fadeToggle(300);
-  });
-
-  $(".dropdown-select-selected.mutli-select-mag .selected").on("click", function () {
-    $(".dropdown-select-options.mutli-select-mag").fadeToggle(300);
-  });
-
-  $(".mutli-select-pub .selection").on("click", function () {
-    // code here
-  });
-
-  $(".mutli-select-mag .selection").on("click", function () {
-    // code here
-  });
+  // code here
 
   // form submit
   $('.comp-catfish-checkout').on('submit', function (e) {
