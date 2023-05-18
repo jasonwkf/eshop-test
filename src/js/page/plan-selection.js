378ed4b8a0df9570c131973d0da45ef8fa44fe75
Yang Gao <yang.gao@sioux.asia>
2023-05-18 17:28:13 +0800

    improve the workflow in responsive

--- src/js/page/plan-selection.js
+++ src/js/page/plan-selection.js
@@ -19,10 +19,29 @@ $(function () {
   $('.plan-cta > .pub-plan-single > .cta-button').on('click', function (e) {
     e.preventDefault();
     const allButtons = document.querySelectorAll('.pub-plan-single > .cta-button.button-text');
-    allButtons.forEach(btn => btn.classList.remove("cta-button-active"));
+    allButtons.forEach(btn => {
+      btn.classList.remove("cta-button-active");
+      btn.innerHTML = "select";
+    });
     e.target.classList.add("cta-button-active");
+    e.target.innerHTML = "selected";
     // SELECT any publication, the step2 part should be active
     $(".section-container > .section-body > .disabled").remove();
+    // auto scroll to step2
+    $('html, body').animate({
+      scrollTop: $('#step-2-desktop').offset().top - 230,
+    }, 1000);
+    // set pub img and name in mobile dropdown
+    const mobilePubOptions = document.querySelectorAll(".dropdown-select-options.mutli-select-pub input");
+    let pubMobileImgSrc = '';
+    mobilePubOptions.forEach(option => {
+      if (option.dataset.label === e.target.dataset.id) {
+        pubMobileImgSrc = option.dataset.img;
+      }
+    });
+    $(".mutli-select-pub .paymentImg").attr("src", pubMobileImgSrc);
+    $('.mutli-select-pub .label.placeholder').text(e.target.dataset.id).css({ color: '#000' });
+    $(".comp.card-plan-single .disabled").remove();
     // Set the Publication Name in the Summary Widget
     $('#pub-item-input').attr("hidden", false).val(e.target.dataset.id).css({
       border: 'none',
@@ -35,8 +54,22 @@ $(function () {
   $('.magazine-plan-single > .cta-button').on('click', function (e) {
     e.preventDefault();
     const allButtons = document.querySelectorAll('.magazine-plan-single > .cta-button.button-text');
-    allButtons.forEach(btn => btn.classList.remove("cta-button-active"));
+    allButtons.forEach(btn => {
+      btn.classList.remove("cta-button-active");
+      btn.innerHTML = "select";
+    });
     e.target.classList.add("cta-button-active");
+    e.target.innerHTML = "selected";
+    // set magazine img and name in mobile dropdown
+    const mobileMagOptions = document.querySelectorAll(".dropdown-select-options.mutli-select-mag input");
+    let magMobileImgSrc = '';
+    mobileMagOptions.forEach(option => {
+      if (option.dataset.label === e.target.dataset.id) {
+        magMobileImgSrc = option.dataset.img;
+      }
+    });
+    $(".mutli-select-mag .paymentImg").attr("src", magMobileImgSrc);
+    $('.mutli-select-mag .label.placeholder').text(e.target.dataset.id).css({ color: '#000' });
     // Show Product Summary Widget
     $(".comp.comp-catfish-checkout").css({
       visibility: 'visible',
@@ -65,6 +98,18 @@ $(function () {
     // fill the selection image and title
     $(".mutli-select-pub .paymentImg").attr("src", e.target.dataset.img);
     $('.mutli-select-pub .label.placeholder').val(e.target.dataset.label).text(e.target.dataset.label).css({ color: '#000' });
+    // set pub selected in desktop
+    const allButtons = document.querySelectorAll('.pub-plan-single > .cta-button.button-text');
+    const pubName = e.target.dataset.label;
+    allButtons.forEach(btn => {
+      btn.classList.remove("cta-button-active");
+      btn.innerHTML = "select";
+      if (btn.dataset.id === pubName) {
+        btn.classList.add("cta-button-active");
+        btn.innerHTML = "selected";
+      }
+    });
+    $(".section-container > .section-body > .disabled").remove();
     $(".dropdown-select-options.mutli-select-pub").fadeToggle(300);
     // Select any one of the publications, the step2 part should be active
     $(".comp.card-plan-single .disabled").remove();
@@ -81,6 +126,17 @@ $(function () {
   $(".mutli-select-mag .selection").on("click", function (e) {
     $(".mutli-select-mag .paymentImg").attr("src", e.target.dataset.img);
     $(".mutli-select-mag .label.placeholder").val(e.target.dataset.label).text(e.target.dataset.label).css({ color: '#000' });
+    // set pub selected in desktop
+    const allButtons = document.querySelectorAll('.magazine-plan-single > .cta-button.button-text');
+    const magName = e.target.dataset.label;
+    allButtons.forEach(btn => {
+      btn.classList.remove("cta-button-active");
+      btn.innerHTML = "select";
+      if (btn.dataset.id === magName) {
+        btn.classList.add("cta-button-active");
+        btn.innerHTML = "selected";
+      }
+    });
     $(".dropdown-select-options.mutli-select-mag").fadeToggle(300);
     // Show Product Summary Widget
     $(".comp.comp-catfish-checkout").css({
@@ -112,7 +168,10 @@ $(function () {
       transform: 'translateY(300px)',
     });
     const allButtons = document.querySelectorAll('.cta-button.button-text');
-    allButtons.forEach(btn => btn.classList.remove("cta-button-active"));
+    allButtons.forEach(btn => {
+      btn.classList.remove("cta-button-active");
+      btn.innerHTML = "select";
+    });
     $('#step-2-desktop .section-body').append('<div class="disabled"></div>');
     // For mobile, there is no 'step-2-mobile' id, so in real develop work, I will add this id and append the disabled area.
     $(".mutli-select-pub .paymentImg").attr("src", "");
