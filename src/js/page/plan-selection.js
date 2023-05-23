f4603dcc57e2259d1eb4915f0f11ff9d1b458569
jingliseah <jingliseah@gmail.com>
2023-05-23 17:12:56 +0800

    feat: desktop card plan selection function

--- src/js/page/plan-selection.js
+++ src/js/page/plan-selection.js
@@ -1,8 +1,10 @@
 $(function () {
+  var mobileScreen = $(window).width() < 992;
+
   // panel interaction
   $('#cta-m-one').on('click', function (e) {
     e.preventDefault();
-    if ($(window).width() < 992) {
+    if (mobileScreen) {
       console.log('trigger');
       $('html, body').animate({
         scrollTop: $('#step-1-mobile').offset().top - 64,
@@ -15,6 +17,71 @@ $(function () {
   });
 
   // code here
+  // step 1 interaction
+  if ($('.pub-plan-single').length > 0) {
+    $('.pub-plan-single').each(function () {
+      var _this = this;
+
+      $(_this).find('.cta-button').on('click', function (e) {
+        // disable redirection
+        e.preventDefault();
+
+        // activate selected button
+        const ctaButtons = $('.pub-plan-single .cta-button');
+        ctaButtons.each(function () {
+          $(this).removeClass('active').text('Select');
+        });
+        $(this).addClass('active').text('Selected');
+
+        // set value to catfish checkout
+        var id = $(this).data('id');
+        $('#pub-item').text(id);
+
+        // anchor to step 2
+        if (mobileScreen) {
+          $('html, body').animate({
+            scrollTop: $('#step-2-mobile').offset().top - 64,
+          }, 1000);
+        } else {
+          $('html, body').animate({
+            scrollTop: $('#step-2-desktop').offset().top - 30,
+          }, 1000, function () {
+            // animation complete
+            $('#step-2-desktop').find('.disabled').fadeOut();
+          });
+        }
+      });
+    });
+  }
+
+  // step 2 interaction
+  if ($('.magazine-plan-single').length > 0) {
+    $('.magazine-plan-single').each(function () {
+      var _this = this;
+
+      $(_this).find('.cta-button').on('click', function (e) {
+        // disable redirection
+        e.preventDefault();
+
+        // activate selected button
+        const ctaButtons = $('.magazine-plan-single .cta-button');
+        ctaButtons.each(function () {
+          $(this).removeClass('active').text('Select');
+        });
+        $(this).addClass('active').text('Selected');
+
+        // set value to catfish checkout
+        var id = $(this).data('id');
+        $('#mag-item').text(id);
+
+        // display checkout
+        $('.comp-catfish-checkout').addClass('slide-up');
+
+        // enabled checkout button
+        $('#pub-submit').removeAttr('disabled');
+      });
+    });
+  }
 
   // form submit
   $('.comp-catfish-checkout').on('submit', function (e) {
@@ -23,7 +90,7 @@ $(function () {
       publication: $(this).find('input').eq(0).val(),
       magazine: $(this).find('input').eq(1).val(),
     };
-    console.lg(postObj, 'Checkout');
+    console.log(postObj, 'Checkout');
   });
 
   // m1 reselect prompt
